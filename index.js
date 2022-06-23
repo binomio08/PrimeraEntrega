eaa9afcb1fc627518e6addf1da59835ac5b039e6
Leandro Troncoso <binomio08@hotmail.com>
2022-06-22 23:25:57 -0300

    primer commit

---
+++ index.js
@@ -0,0 +1,21 @@
+//VARIABLES
+let luz = 3000
+let gas = 2500
+let arba = 1000
+let telefono =990
+
+//FUNCIONES
+function nombrecliente(){
+    alert("Bienvenido a Pago tus Cuentas")
+    let nombreCliente = prompt("Ingrese su Alias de Cliente")
+    if(nombreCliente === ""){
+        let nombreCliente = prompt("Ingrese su Alias de Cliente")
+    }
+    
+}
+nombrecliente()
+function menu(){
+    let opcion = prompt("Bienvenido a PAGO MIS CUENTAS\n Seleccione que servicios quiere pagar (ESC) pasa salir: \n1. Luz\n2. Gas\n3. Arba\n4. Teléfono");
+
+}
+menu()
